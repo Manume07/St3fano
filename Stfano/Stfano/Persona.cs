4da64e344f7cdba9963026642f76638166683011
Manume_ <clash41mini@gmail.com>
2025-11-20 17:12:12 +0100

    R1.0 modificata la classe persona

--- Stfano/Stfano/Persona.cs
+++ Stfano/Stfano/Persona.cs
@@ -8,5 +8,16 @@ namespace Stfano
 {
     internal class Persona
     {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public Persona(string _nome, string _cognome)
+        {
+            Nome = _nome;
+            Cognome = _cognome;
+        }
+        public string StampaNomeCompleto()
+        {
+            return $"{Nome} {Cognome}";
+        }
     }
 }
